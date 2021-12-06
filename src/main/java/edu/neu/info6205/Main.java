84b30ea46b2181061218da0127cf0106e1b6071e
jamesshah <jdshah18072000@gmail.com>
2021-12-05 23:25:22 -0500

    modified report to include output

--- src/main/java/edu/neu/info6205/Main.java
+++ src/main/java/edu/neu/info6205/Main.java
@@ -4,7 +4,6 @@ import edu.neu.info6205.util.ReadFile;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 
 /**
  * Main class that works as an entry point for benchmarking various algorithms with various sized elements.
@@ -13,27 +12,32 @@ public class Main {
 
     private static String filePath = "./src/main/resources/shuffledHindi.txt";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         /**
-//         * Array of various sizes for benchmarking array
-//         */
-        int[] sizesOfArray = {8000, 16000, 32000, 64000, 128_000, 256_000,
-                        512_000, 1000_000, 2000_000, 4000_000, 8000_000, 16000_000};
-
-        // Running benchmarks for various size of array
-        for (int size: sizesOfArray) {
-
-            System.out.println("\n" + "Array size: " + size + "\n");
-
-            String[] arr = ReadFile.getExtendedStringArray(filePath, size);
-
-            assert arr.length == size : "Size of the 'arr' array doesn't match with expected size";
-//            BenchmarkDriver.runBenchmark("MSDO", () -> arr, 10);
-            BenchmarkDriver.runBenchmark("MSD", () -> arr, 10);
+         * Array of various sizes for benchmarking array
+         */
+//        int[] sizesOfArray = {8000, 16000, 32000, 64000, 128_000, 256_000,
+//                        512_000, 1000_000, 2000_000, 4000_000, 8000_000, 16000_000};
+//
+//        // Running benchmarks for various size of array
+//        for (int size: sizesOfArray) {
+//
+//            System.out.println("\n" + "Array size: " + size + "\n");
+//
+//            String[] arr = ReadFile.getExtendedStringArray(filePath, size);
+//
+//            assert arr.length == size : "Size of the 'arr' array doesn't match with expected size";
+//            BenchmarkDriver.runBenchmark("MSD", () -> arr, 10);
 //            BenchmarkDriver.runBenchmark("Tim", () -> arr, 10);
 //            BenchmarkDriver.runBenchmark("LSD", () -> arr, 10);
 //            BenchmarkDriver.runBenchmark("Husky", () -> arr, 10);
 //            BenchmarkDriver.runDPQBenchMark(filePath, size, 10);
-        }
+
+        int size = 38000;
+        String[] arr = ReadFile.getExtendedStringArray(filePath, size);
+        MSDRadixSort.sort(arr);
+        for (int i = 0; i < 100; i++)
+            System.out.print(arr[i] + ", ");
+
     }
 }
